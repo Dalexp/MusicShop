74ff9131fa863cdcb0a45702af829c3ba86fda2e
Dalexp <wertyu183@yandex.ru>
2020-01-20 17:59:13 +0300

    add ArrayList,add spinner,adapter,refactor,add switch

--- app/src/main/java/com/example/musicshop/MainActivity.java
+++ app/src/main/java/com/example/musicshop/MainActivity.java
@@ -1,29 +1,71 @@
 package com.example.musicshop;
-
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.Spinner;
 import android.widget.TextView;
+import java.util.ArrayList;
+import java.util.HashMap;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
     int quantity=0;
+    Spinner spinner;
+    ArrayList <String> spinnerArrayList;
+    ArrayAdapter spinnerAdapter;
+
+    HashMap <String,Double> goodsMap;
+    String goodsName;
+    double price;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        spinner=findViewById(R.id.spinner);
+        spinner.setOnItemSelectedListener(this);
+        spinnerArrayList = new ArrayList<>();
+
+        createSpinner();
+        createMap();
+
+    }
+    @Nullable
+    void createSpinner(){
+        spinnerArrayList.add("guitar");
+        spinnerArrayList.add("drums");
+        spinnerArrayList.add("keyboard");
+
+        spinnerAdapter = new ArrayAdapter(this,android.R.layout.simple_spinner_item,spinnerArrayList);
+        spinnerAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(spinnerAdapter);
     }
 
+    void createMap(){
+        goodsMap = new HashMap<>();
+        goodsMap.put("guitar",500.0);
+        goodsMap.put("drums",800.0);
+        goodsMap.put("keyboard",1200.0);
+    }
 
     public void increaseQuantity(View view) {
         quantity+=1;
         if(quantity>9){
             quantity=9;
         }
+        String operation=""+quantity;
+        String operationPrice="" + quantity * price;
+
         TextView quantityTextView = findViewById(R.id.quantityTextView);
-        quantityTextView.setText(""+ quantity);
+        quantityTextView.setText(operation);
+
+        TextView priceTextView=findViewById(R.id.priceTextView);
+        priceTextView.setText(operationPrice);
     }
 
     public void decreaseQuantity(View view) {
@@ -31,7 +73,47 @@ public class MainActivity extends AppCompatActivity {
         if(quantity<0){
             quantity=0;
         }
+        String operation=""+quantity;
+        String operationPrice="" + quantity * price;
+
         TextView quantityTextView = findViewById(R.id.quantityTextView);
-        quantityTextView.setText("" + quantity);
+        quantityTextView.setText(operation);
+
+        TextView priceTextView=findViewById(R.id.priceTextView);
+        priceTextView.setText(operationPrice);
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        String operationPrice="" + quantity * price;
+
+        goodsName=spinner.getSelectedItem().toString();
+        price = goodsMap.get(goodsName);
+
+        TextView priceTextView=findViewById(R.id.priceTextView);
+        priceTextView.setText(operationPrice);
+
+        ImageView goodsImageView=findViewById(R.id.guitarImage);
+
+        switch (goodsName)
+        {
+            case "guitar":
+                goodsImageView.setImageResource(R.drawable.guitar);
+                break;
+            case "drums":
+                goodsImageView.setImageResource(R.drawable.drums);
+                break;
+            case "keyboard":
+                goodsImageView.setImageResource(R.drawable.keyboard);
+                break;
+            default:
+                goodsImageView.setImageResource(R.drawable.guitar);
+                break;
+        }
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
     }
 }
