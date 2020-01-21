878af3d4f513e1729e887578bb724dc3466008e8
Dalexp <wertyu183@yandex.ru>
2020-01-21 16:40:43 +0300

    refactor

--- app/src/main/java/com/example/musicshop/MainActivity.java
+++ app/src/main/java/com/example/musicshop/MainActivity.java
@@ -1,10 +1,8 @@
 package com.example.musicshop;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
